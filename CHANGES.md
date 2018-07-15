21bc14542d695b1ac8c05802a9c3612799ef46a7
David Mudrák <david@moodle.com>
2018-07-15 14:14:38 +0200

    Call it 1.1.0

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,12 @@
 # CHANGES #
 
+## 1.1.0 ##
+
+* Privacy API implementation.
+* Minor usability improvement - display a hint if the Add button is pressed without
+  todo item text provided.
+* The plugin is considered to be stable now.
+
 ## 1.0.0 ##
 
 * Initial implementation of the plugin, first public release.
--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_todo';
-$plugin->release = '1.0.0';
-$plugin->version = 2018010503;
+$plugin->release = '1.1.0';
+$plugin->version = 2018071500;
 $plugin->requires = 2017051500;
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_STABLE;
